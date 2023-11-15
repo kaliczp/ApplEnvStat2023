fa4eb4175c7be8f4f957d3297b0d410641beda7f
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-11-15 09:21:32 +0100

    different means

--- tests.R
+++ tests.R
@@ -7,4 +7,8 @@ mean(pH1);mean(pH2)
 
 ## Find tests
 apropos("test")
-t.test(pH1,pH2)
+t.test(pH1,pH2) ## p-value is high -> accept
+
+pH4 <- rnorm(50, 7, 0.3)
+pH5 <- rnorm(50, 6, 0.3)
+t.test(pH4,pH5)
