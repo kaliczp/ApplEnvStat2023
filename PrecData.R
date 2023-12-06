9056a75b076ffba6874d0ccfcf72a6959d267f2c
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-12-06 09:35:04 +0100

    See time-data plot

--- PrecData.R
+++ PrecData.R
@@ -6,3 +6,18 @@ summary(Sop.Prec)
 head(Sop.Prec)
 ## Header is exists
 Sop.Prec <- read.table("r_h_Sopron_19012021.csv", sep = ";", head = TRUE)
+## Explore data
+boxplot(Sop.Prec$r)
+qqnorm(Sop.Prec$r)
+qqline(Sop.Prec$r)
+plot(density(Sop.Prec$r))
+plot(density(Sop.Prec$r), xlim=c(0,3))
+
+## Time-series
+install.packages("xts")
+library(xts)
+## Generate dates for dataset
+SopDate <- as.Date("1901-01-01")+0:44194
+SopDate <- seq(as.Date("1901-01-01"), as.Date("2021-12-31"), "day")
+summary(SopDate)
+plot(SopDate, Sop.Prec$r, typ="h")
