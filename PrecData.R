796516c07c06214d25b4f6bbd563bd43fc9cf025
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-12-06 09:08:58 +0100

    Import precipitation data

---
+++ PrecData.R
@@ -0,0 +1,4 @@
+## Download some dataset from odp.met.hu
+## Unzip into the folder for the course
+## Import into your workspace based on R Data Import/Export manual 2nd section
+Sop.Prec <- read.table("r_h_Sopron_19012021.csv", sep = ";")
